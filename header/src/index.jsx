5ca8e973e476d12033e48b204f4d4bf9ad77bfc4
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-14 13:18:47 +0530

    index

---
+++ header/src/index.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import App from './Components/App';
+import theme from './Components/theme'; 
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
